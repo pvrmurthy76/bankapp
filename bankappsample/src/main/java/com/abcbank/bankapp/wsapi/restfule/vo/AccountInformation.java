9374b6999e2a5cae48652f5208ac307042d6c7af
pvrmurthy76 <51827595+pvrmurthy76@users.noreply.github.com>
2021-04-22 16:30:04 +0530

    Update AccountInformation.java

--- bankappsample/src/main/java/com/abcbank/bankapp/wsapi/restfule/vo/AccountInformation.java
+++ bankappsample/src/main/java/com/abcbank/bankapp/wsapi/restfule/vo/AccountInformation.java
@@ -2,25 +2,24 @@ package com.abcbank.bankapp.wsapi.restfule.vo;
 
 import java.util.Date;
 
+import com.abcbank.bankapp.service.util.TransactionType;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class AccountInformation {
+public class AccountInformation extends CustomerInformation{
 
-    private String name;
-
-    private String contactNumber;
-
-    private AddressInformation customerAddress;
-
+    @NotNull
     private Long accountNumber;
 
     private BankInformation bankInformation;
